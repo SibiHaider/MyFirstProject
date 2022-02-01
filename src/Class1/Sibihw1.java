2e321cd4b22f92a2bb80bb118c6087a2b3c88249
sibihaider <sibi.haider110@gmail.com>
2022-02-01 03:47:27 -0500

    My First HW

---
+++ src/Class1/Sibihw1.java
@@ -0,0 +1,64 @@
+package Class1;
+import java.lang.invoke.MethodHandle;
+
+public class Sibihw1 {
+
+    public static void main(String[] args) {
+
+        // HW number 1
+
+        byte hoursInDay = 24;
+        System.out.println(" There are " + hoursInDay + "hrs in a day.");
+
+        short DaysInYear = 365;
+        System.out.println("\n There are " + DaysInYear + " days in a year.");
+
+        int employeesInGoogle = 35000;
+        System.out.println("\n There are a total of " +employeesInGoogle + " employees working at google.");
+
+        long PopulationInIndia = 1400000000000L;
+        System.out.println("\n The population of india as of 2022 is " +PopulationInIndia + ".");
+
+        float mortgageRate = 4.02f;
+        System.out.println("\n " + mortgageRate + "% is the current mortgage rate as of 2022.");
+
+        double BankBalance = 189890.99;
+        System.out.println("\n Your current balance is $" + BankBalance);
+
+
+        boolean doesTheSunRiseFromTheWest = false;
+        System.out.println("\n Does the sun rise from the west? : " + doesTheSunRiseFromTheWest);
+
+        char initial1 = 'M';
+        char initial2 = 'S';
+        char initial3 = 'H';
+        System.out.println("\n My initials are: " + initial1 + initial2 + initial3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
