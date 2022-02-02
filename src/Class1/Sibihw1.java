eeb9c817bd7dad9c1dd00cf031ce3e5c2eeb6c09
sibihaider <sibi.haider110@gmail.com>
2022-02-01 19:16:38 -0500

    My First Hw

--- src/Class1/Sibihw1.java
+++ src/Class1/Sibihw1.java
@@ -34,6 +34,38 @@ public class Sibihw1 {
         char initial3 = 'H';
         System.out.println("\n My initials are: " + initial1 + initial2 + initial3);
 
+        // HomeWork 2
+        /** T(°F) = T(°C) × 9/5 + 32
+         *  T(K) = T(°C) + 273.15
+         *
+         *  T(°C) = (T(°F) - 32) × 5/9
+         *  T(K) = (T(°F) + 459.67) × 5/9
+         *
+         *  T(°C) = T(K) - 273.15
+         *  T(°F) = T(K) × 9/5 - 459.67
+         *
+         */
+
+        // Celsius to Fahrenheit (°F) = (°C) × 9/5 + 32
+        double cTemp = 25;
+        double fTemp = cTemp * 9/5 + 32;
+        System.out.println("\n"+ cTemp + "°C = " + fTemp + "°F");
+
+        // Celsius to Kelvin (K) = (°C) + 273.15
+        double cTemp1 = 34;
+        double kTemp= cTemp1 + 273.15;
+        System.out.println("\n" + cTemp1 + "°C = " + kTemp + "K");
+
+        // Fahrenheit to Celsius (°C) = (°F) - 32) × 5/9
+        double fTemp1 = 30;
+        double cTemp2 = (fTemp1 - 32) * 5/9;
+        System.out.println("\n"+ fTemp1 + "°F = " + cTemp2 + "°C");
+
+        // Fahrenheit to Kelvin (K) = (°F) + 459.67) × 5/9
+        double fTemp2 = 18f;
+        double kTemp1 = (fTemp2 + 459.67) * 5/9;
+        System.out.println("\n" + fTemp2 + "°F = " + kTemp1 + "K");
+
 
 
     }
